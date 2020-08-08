a4a3eb74a23b836de1761206efba900f31a24938
yavivi <yotam.avivi@gmail.com>
2020-08-09 00:02:03 +0300

    Changed package.json - start to run both server and client

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload me
+          Edit <code>src/App.js</code> and save to reload me!
         </p>
         <a
           className="App-link"
