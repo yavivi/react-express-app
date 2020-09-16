e9419104bcc51770fbea1044461217e97ff0d01a
Yaron <yaronavraham600@gmail.com>
2020-09-16 12:57:04 +0300

    add object "yaron avraham "

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,12 @@ router.get('/', function(req, res, next) {
   {
     "id": 2,
     "name": "Jane Doe"
-  }];
+  },
+  {
+    "id": 77,
+    "name": "yaron avraham"
+  }
+];
   res.send(users);
 });
 
