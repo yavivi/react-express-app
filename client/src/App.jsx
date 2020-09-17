0a13e19c3f4b9ee697fac9b9f388bb2f76788ab6
Yaron <yaronavraham600@gmail.com>
2020-09-17 18:13:56 +0300

    change domain in app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ function App() {
 
   useEffect(() => {
     // Address should be replaced with FQDN of application and taken from ENV
-    const domain = process.env.REACT_APP_DOMAIN;
+    // const domain = process.env.REACT_APP_DOMAIN;
+    const domain = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DOMAIN : "localhost:3001";
     const http = process.env.NODE_ENV === 'production' ? 'https' : 'http';
     fetch(`${http}://${domain}/users`, {
       headers: {
