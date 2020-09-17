cb2bbf6417514a91c1e4c3094078fccfd4ed0ed0
Yaron <yaronavraham600@gmail.com>
2020-09-17 18:06:47 +0300

    create guide collection

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const db=require('./db/index')
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const guidesRouter = require('./routes/guide-router');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,6 +28,7 @@ app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', guidesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect('mongodb://localhost:27017/Football', { useNewUrlParser: true })
+    .connect( "mongodb+srv://yaron93:102030ya@cluster0.5fsow.mongodb.net/Tilok?retryWrites=true&w=majority ",{ useNewUrlParser: true,useUnifiedTopology:true })
     .catch(e => {
         console.error('Connection error', e.message)
     });
