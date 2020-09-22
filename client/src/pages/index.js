f9fba010d93aa842c19b6123d2676b654bc9b35c
yakov avraham <yakov.avra@gmail.com>
2020-09-22 21:38:09 +0300

    add pages and routing in client

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Homepage from './Homepage';
+import LoginPage from './LoginPage';
+import PublishTicket from './PublishTicket';
+import SellerDashboard from './SellerDashboard';
+import TicketManagement from './TicketManagement';
+import SignupPage from './SignupPage';
+
+export { Homepage, LoginPage, PublishTicket, SellerDashboard, TicketManagement, SignupPage };
