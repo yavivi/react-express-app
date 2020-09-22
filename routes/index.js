1b73073a40bfad1cd8e1d99e92330d3033625ddf
yakov avraham <yakov.avra@gmail.com>
2020-09-22 22:23:20 +0300

    add static.json file in client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
