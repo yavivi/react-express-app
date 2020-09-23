044320c84178d8d58b45bad79f34513ab63d2517
yakov avraham <yakov.avra@gmail.com>
2020-09-23 15:31:43 +0300

    get all tickets from database

--- client/src/components/TicketsContainer/index.js
+++ client/src/components/TicketsContainer/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import DemoTicket from '../DemoTicket';
+import {getAllTickets} from '../../apis/ticketsApi';
 
 export default function TicketsContainer() {
-    const tickets = [
-        { _id: 1, ticket_title: 'Beyonce' },
-        { _id: 2, ticket_title: 'Omer Adam' },
-        { _id: 3, ticket_title: 'Tyga' },
-        { _id: 4, ticket_title: 'Beyonce' },
-        { _id: 5, ticket_title: 'Omer Adam' },
-        { _id: 6, ticket_title: 'Tyga' }
-    ];
+    const [tickets, setTickets] = useState([]);
+
+    useEffect(() => {
+        getAllTickets()
+            .then(res => setTickets(res.data.data))
+            .catch(error => console.log(error));
+    }, []);
 
     return (
         <div className="tickets-container">
