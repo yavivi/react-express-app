0d207f9955b15d0e7fefff5ae1b02de6dfc6bc76
megi mentesnot <megiment@gmail.com>
2020-09-24 19:07:35 +0300

    date

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import './CardComponent.css';
 
 
+export default function CardComponent({ eventInfo }) {
+const dateObject = new Date(eventInfo.ticket_dates);
+const date = dateObject.toLocaleDateString();
+const time = dateObject.toLocaleTimeString();
 
-export default function CardComponent({eventInfo}) {
 
     return (
+
         <div className="card ticket" >
             <div className="eventImgContainer">
                 <img className="eventImg" src={eventInfo.imgUrl} alt="event" />
             </div>
 
             <p className="eventName">{eventInfo.ticket_title}</p>
-            <span className="date">{eventInfo.ticket_dates/*.toLocaleDateString()*/} </span>
+            <span className="date">{date} </span>
             |
-            <span className="hour"> {eventInfo.hour/*.toLocaleTimeString()*/}</span>  
+            <span className="hour">{time}</span>
             <p className="location">{eventInfo.location}</p>
             <p className="sellerName">{eventInfo.seller_Id}</p>
             <p className="price">{eventInfo.price} ₪</p>
