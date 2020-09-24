9c501c7752d7eb9d3261ef58440d4f5e3fd025ae
yakov avraham <yakov.avra@gmail.com>
2020-09-25 01:17:25 +0300

    add hover text to logo

--- client/src/components/MainNavbar/index.jsx
+++ client/src/components/MainNavbar/index.jsx
@@ -4,9 +4,9 @@ import Logo from '../Logo'
 
 export default function MainNavbar() {
     return (
-        <div className="main-navbar">
+        <nav className="main-navbar">
             <Logo />
             {window.location.pathname === '/' && <LinkButton className="sell-tickets-btn" url="/sellers/login" text="SELL TICKETS"/>}
-        </div>
+        </nav>
     )
 }
