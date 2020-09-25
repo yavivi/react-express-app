c0c6f1a65b37c48f6f094e9d4db28e99ef506fc2
stam <simchabalay@gmail.com>
2020-09-25 18:02:56 +0300

    form fileds sell tick

--- client/src/components/Sell/TimePicker.js
+++ client/src/components/Sell/TimePicker.js
@@ -1,52 +1,79 @@
-// import React, { Component } from 'react'
-// import Timepicker from 'react-timepicker';
-// import 'react-datepicker/dist/react-datepicker.css';
+import React, { Component } from 'react'
+class SellTickets extends Component {
+  constructor() {
+    super();
+    this.state = {
+      category_Id: '',
+      location: '',
+      eventName: '',
+      time: '',
+      ticket_dates: '',
+      price: '',
+      seats:'',
+      row:"",
+      seller_Id:"",
+      description:"",
+    };
+    this.dismissError = this.dismissError.bind(this);
+  }
 
+  dismissError() {
+    this.setState({ error: '' });
+  }
 
-// class Time extends Component {
-//     constructor() {
-//         super()
-//         this.state = {
+  render() {
+      return (
+      <div className="SellTicket">
+       {/* <form onSubmit={this.handleSubmit}> */}
+       <form>
+           <h1>Sell Ticket Page</h1>
 
-//             time: "",
-//         }
-//     }
+          <label for="chooseCategory">Category:</label>
+          <input placeholder="choose category"  type="text"/>
 
-//     componentDidMount() {
-//         document.addEventListener('DOMContentLoaded', ()=> {
-//             var elems = document.querySelectorAll('.timepicker');
-//             var instances = M.Timepicker.init(elems, options);
-//           });
-//     }
-//     render() {
-//         return (
-//             <div className="row">
-//                 <div className="input-filed col s12">
-//                 <input type="text" class="timepicker"/>
-//                     <label htmlFor="time" className="active">
-//                         Time
-//                     </label>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-// export default Time
-import React, { useState } from "react";
-import { KeyboardTimePicker } from "@material-ui/pickers";
+          <label for="location">Location:</label>
+          <input placeholder="location" type="text"/>
 
-function KeyboardTimePickerExample() {
-  const [selectedDate, handleDateChange] = useState(new Date());
+          <label for="eventName">Event Name:</label>
+          <input placeholder="eventName" data-test="eventName" type="text"/> 
 
-  return (
-    <KeyboardTimePicker
-      label="Masked timepicker"
-      placeholder="08:00 AM"
-      mask="__:__ _M"
-      value={selectedDate}
-      onChange={date => handleDateChange(date)}
-    />
-  );
+          {/* <label for="ticket_dates">time:</label> */}
+
+          <label for="appt">Select a time:</label>
+          <input type="time" id="appt" />
+          <button  type="submit">Done</button>
+          {/* <input placeholder="time" type="text"  value={this.state.time}/> */}
+
+          <label for="ticket_dates">ticket_dates:</label>
+          <input placeholder="ticket_dates" type="text" value={this.state.ticket_dates}/>
+
+          <label for="price">Enter the price:</label>
+          <input placeholder="price" type="text" /> 
+
+          <label for="seats">Enter the seat:</label>
+          <input placeholder="seats" type="text" />
+
+          <label for="row">Enter the row of seat:</label>
+          <input placeholder="row" type="text"/>
+
+          <label for="desseller_Idcription">Seller Id:</label>
+          <input placeholder="seller_Id" type="text"/>//from dataBase
+
+          <label for="description">description:</label>
+          <textarea name="comment" >Enter text here...</textarea>    
+
+          <button  type="submit">Done</button>
+          {
+            
+            this.state.error &&
+            <h3 data-test="error" onClick={this.dismissError}>
+              <button onClick={this.dismissError}>✖</button>
+              {this.state.error}
+            </h3>
+          }
+        </form>
+      </div>
+    );
+  }
 }
-
-export default KeyboardTimePickerExample;
+export default SellTickets;
